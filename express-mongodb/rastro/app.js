7957e458f34de63d72774e549197901c8d4809e8
Jessica Rodrigues <jessirodrigues0501@gmail.com>
2021-06-03 15:04:37 -0300

    criando uma rota

--- express-mongodb/rastro/app.js
+++ express-mongodb/rastro/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
-console.log(`express: ${typeof(express)} | constructor: ${express.constructor.name}`);
+// console.log(`express: ${typeof(express)} | constructor: ${express.constructor.name}`);
 
 const app = express();
-console.log(`app: ${typeof(app)} | constructor: ${app.constructor.name}`);
+// console.log(`app: ${typeof(app)} | constructor: ${app.constructor.name}`);
 
 app.listen(3000/*porta*/, () => console.log('Servidor rodando na porta 3000...'));
+
+//criando uma rota
+
+//get, define a rota e a função que deve ser executada...
+app.get(
+    '/', //é a URL da rota
+    (request, response) => {
+        console.log('Rota principal chamada...');
+        response.send('Servidor rodando, está tudo ok');
+    }
+);
