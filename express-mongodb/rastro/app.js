6af12382b5d5d3ba8d72d3f82fdfc343a611c2bf
Jessica Rodrigues <jessirodrigues0501@gmail.com>
2021-06-03 12:57:53 -0300

    atualização servidor app

--- express-mongodb/rastro/app.js
+++ express-mongodb/rastro/app.js
@@ -1,1 +1,7 @@
 const express = require('express');
+console.log(`express: ${typeof(express)} | constructor: ${express.constructor.name}`);
+
+const app = express();
+console.log(`app: ${typeof(app)} | constructor: ${app.constructor.name}`);
+
+app.listen(3000/*porta*/, () => console.log('Servidor rodando na porta 3000...'));
