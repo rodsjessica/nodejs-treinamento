bc2b9f49b3d07839e4de00e410f216bac4c79f3c
Jessica Rodrigues <jessirodrigues0501@gmail.com>
2021-06-03 16:15:23 -0300

    criando rota para cadastrar o rastreador

--- express-mongodb/rastro/app.js
+++ express-mongodb/rastro/app.js
@@ -4,6 +4,10 @@ const express = require('express');
 const app = express();
 // console.log(`app: ${typeof(app)} | constructor: ${app.constructor.name}`);
 
+//essas configurações vão permitir acessar o body do request
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.listen(3000/*porta*/, () => console.log('Servidor rodando na porta 3000...'));
 
 //criando uma rota
@@ -14,5 +18,16 @@ app.get(
     (request, response) => {
         console.log('Rota principal chamada...');
         response.send('Servidor rodando, está tudo ok');
+    }
+);
+
+//criando rota para cadastrar rastreador
+app.post(
+    '/rastreador',
+    (request, response) => {
+        console.log('Rota /rastreador chamada...');
+        console.log(`request.body: ${request.body}`);
+        console.log(request.body);
+        response.send('OK');
     }
 );
